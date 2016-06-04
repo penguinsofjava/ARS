dcfd4005f43272f6558a8ae4061b4fbc727f3a64
Ihsan Isik <x.ihsan@gmail.com>
2016-06-04 16:09:26 +0300

    Indent RadarMath and remove some empty newlines

--- src/sample/logic/RadarMath.java
+++ src/sample/logic/RadarMath.java
@@ -1,54 +1,44 @@
 package sample.logic;
 
-import java.util.Random;
-
 import sample.model.Plane;
 import sample.model.Radar;
 
 public class RadarMath {
-	
-	/*
-	 * The function controls the 2 statement. If the coordinates of the plane satisfy the circle equation
-	 * AND corrects the angle equation, it means that the plane is in the radar.
-	 * 
-	 */
-	public static boolean isPlaneInTheRadar(Plane plane, Radar radar){
-		return isPlaneInTheAngle(plane, radar);
-	}
 
-	/*
-	 * This function controls whether coordinates of plane satisfy the circle equation or not.
-	 * If they satisfy, the function returns TRUE, else FALSE.
-	 */
-	public static boolean isPlaneInTheCircle(Plane plane, Radar radar){
-		
-		//General circle equation. r^2 = (x^2+y^2)
-		double xSideOfTheEquation = (plane.getPosition().getX() - radar.getPosition().getX())*(plane.getPosition().getX() - radar.getPosition().getX());
-		double ySideOFTheEquation = (plane.getPosition().getY() - radar.getPosition().getY())*(plane.getPosition().getY() - radar.getPosition().getY());
-		double radarRadius = radar.getRadius();
-		
-		return xSideOfTheEquation + ySideOFTheEquation <= Math.pow(radarRadius, 2);
-	}
+    /*
+     * The function controls the 2 statement. If the coordinates of the plane satisfy the circle equation
+     * AND corrects the angle equation, it means that the plane is in the radar.
+     *
+     */
+    public static boolean isPlaneInTheRadar(Plane plane, Radar radar) {
+        return isPlaneInTheAngle(plane, radar);
+    }
 
-	
-	/*
-	 * This function checks if the points of the plane in the given angle or not.
-	 * If so returns TRUE, else FALSE.
-	 */
-	public static boolean isPlaneInTheAngle(Plane plane, Radar radar) {
+    /*
+     * This function controls whether coordinates of plane satisfy the circle equation or not.
+     * If they satisfy, the function returns TRUE, else FALSE.
+     */
+    public static boolean isPlaneInTheCircle(Plane plane, Radar radar) {
+        //General circle equation. r^2 = (x^2+y^2)
+        double xSideOfTheEquation = (plane.getPosition().getX() - radar.getPosition().getX()) * (plane.getPosition().getX() - radar.getPosition().getX());
+        double ySideOFTheEquation = (plane.getPosition().getY() - radar.getPosition().getY()) * (plane.getPosition().getY() - radar.getPosition().getY());
+        double radarRadius = radar.getRadius();
 
-		double planeX = plane.getPosition().getX() - radar.getPosition().getX();
-		double planeY = -1 * plane.getPosition().getY() + radar.getPosition().getY();
+        return xSideOfTheEquation + ySideOFTheEquation <= Math.pow(radarRadius, 2);
+    }
 
+    /*
+     * This function checks if the points of the plane in the given angle or not.
+     * If so returns TRUE, else FALSE.
+     */
+    public static boolean isPlaneInTheAngle(Plane plane, Radar radar) {
+        double planeX = plane.getPosition().getX() - radar.getPosition().getX();
+        double planeY = -1 * plane.getPosition().getY() + radar.getPosition().getY();
 
-		double planeTheta = Math.toRadians(160) + Math.atan(planeY / planeX);
-		/*double planeTheta = planeY <= 0
-				? Math.atan(Math.toRadians(planeY / planeX))
-				: + Math.atan(Math.toRadians(planeY / planeX));
-*/
-		double radarAlpha = Math.toRadians(radar.getScanAngleAlpha());
-		double radarBeta = Math.toRadians(radar.getScanAngle());
+        double planeTheta = Math.toRadians(160) + Math.atan(planeY / planeX);
+        double radarAlpha = Math.toRadians(radar.getScanAngleAlpha());
+        double radarBeta = Math.toRadians(radar.getScanAngle());
 
-		return planeTheta <= (radarAlpha + radarBeta) && planeTheta >= radarAlpha;
-	}
+        return planeTheta <= (radarAlpha + radarBeta) && planeTheta >= radarAlpha;
+    }
 }
