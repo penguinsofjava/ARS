78615e54a822ebe81917b1e96b6b7e5564c5575e
Tunc Guclu <guclutunc@gmail.com>
2016-06-11 14:52:00 +0300

    InterceptorPlane and InterceptorTrackTask classes added

--- src/sample/task/TrackTask.java
+++ src/sample/task/TrackTask.java
@@ -5,23 +5,33 @@ import sample.model.Missile;
 import sample.model.Plane;
 import sample.model.Position;
 
-public class TrackTask {
+import java.util.TimerTask;
 
-    public static void trackPlaneWithMissile(Plane plane,Radar radar){
+public class TrackTask extends TimerTask{
+
+    private Radar radar;
+    private Plane plane;
+
+    public TrackTask(Radar radar, Plane plane) {
+        this.radar = radar;
+        this.plane = plane;
+    }
+
+    private void trackPlaneWithMissile(Plane plane, Radar radar){
         Missile missile = new Missile(plane.getSpeed()*10,new Position(radar.getPosition().getX(),radar.getPosition().getY()));
         int distance;
         do{
             distance=calculateDistance(missile,plane);
             if(missile.getPosition().getX() < plane.getPosition().getX()){
-                missile.getPosition().setX(missile.getPosition().getX()+plane.getSpeed()*2);
+                missile.getPosition().setX(missile.getPosition().getX()+missile.getSpeed());
             }else{
-                missile.getPosition().setX(missile.getPosition().getX()-plane.getSpeed()*2);
+                missile.getPosition().setX(missile.getPosition().getX()-missile.getSpeed());
             }
 
             if(missile.getPosition().getY()<plane.getPosition().getY()){
-                missile.getPosition().setY(missile.getPosition().getY()+plane.getSpeed()*2);
+                missile.getPosition().setY(missile.getPosition().getY()+missile.getSpeed());
             }else{
-                missile.getPosition().setY(missile.getPosition().getY()-plane.getSpeed()*2);
+                missile.getPosition().setY(missile.getPosition().getY()-missile.getSpeed());
             }
         }while(distance > 50);
 
@@ -29,12 +39,17 @@ public class TrackTask {
             missile.getPosition().setX(plane.getPosition().getX());
             missile.getPosition().setY(plane.getPosition().getY());
         }
+
+
     }
 
-
-
-    public static int calculateDistance(Missile missile,Plane plane){
+    private int calculateDistance(Missile missile,Plane plane){
         int distance = (int) Math.sqrt((missile.getPosition().getX()-plane.getPosition().getX())*(missile.getPosition().getX()-plane.getPosition().getX())+missile.getPosition().getY()-plane.getPosition().getY()*missile.getPosition().getY()-plane.getPosition().getY());
         return distance;
+    }
+
+    @Override
+    public void run() {
+        trackPlaneWithMissile(plane, radar);
     }
 }
