a356ae3ded93b51ecb55263d9a73caaf2fe923a3
Tunc Guclu <guclutunc@gmail.com>
2016-06-17 15:46:29 +0300

    InterceptorTrackTask now returns interceptor to base

--- src/sample/task/InterceptorTrackTask.java
+++ src/sample/task/InterceptorTrackTask.java
@@ -1,6 +1,7 @@
 package sample.task;
 
 import sample.coordinator.PlaneCoordinator;
+import sample.model.InterceptorPlane;
 import sample.model.Plane;
 import sample.view.PlaneView;
 
@@ -18,19 +19,53 @@ public class InterceptorTrackTask extends TimerTask {
         this.target = target;
         this.interceptor = interceptor;
     }
+    boolean track = true;
 
     private void trackWithInterceptor() {
-        if (calculateDistance() > 10) {
+
+        if(track){
+            if (calculateDistance() > 10) {
+                int interceptorX = interceptor.getPosition().getX();
+                int interceptorY = interceptor.getPosition().getY();
+                int speed = interceptor.getPlane().getSpeed();
+                interceptor.setX(interceptorX < target.getPosition().getX() ? interceptorX + speed : interceptorX - speed);
+                interceptor.setY(interceptorY < target.getPosition().getY() ? interceptorY + speed : interceptorY - speed);
+            } else {
+                PlaneCoordinator.notifyPlaneTakenDown(target);
+                PlaneCoordinator.removePlane(target);
+                track = false;
+            }
+        }else{
+            returnToBase();
+        }
+
+    }
+
+    private void returnToBase(){
+        if(calculateDistanceToAirBase()>5){
+            int baseX = ((InterceptorPlane) interceptor.getPlane()).getAirbase().getPosition().getX();
+            int baseY = ((InterceptorPlane) interceptor.getPlane()).getAirbase().getPosition().getY();
+            int speed = interceptor.getPlane().getSpeed();
             int interceptorX = interceptor.getPosition().getX();
             int interceptorY = interceptor.getPosition().getY();
-            int speed = interceptor.getPlane().getSpeed();
-            interceptor.setX(interceptorX < target.getPosition().getX() ? interceptorX + speed : interceptorX - speed);
-            interceptor.setY(interceptorY < target.getPosition().getY() ? interceptorY + speed : interceptorY - speed);
-        } else {
-            PlaneCoordinator.notifyPlaneTakenDown(target);
-            PlaneCoordinator.removePlane(target);
+
+            if(interceptorX > baseX){
+                interceptor.setX(interceptorX - speed);
+            }else{
+                interceptor.setX(interceptorX + speed);
+            }
+
+            if(interceptorY > baseY){
+                interceptor.setY(interceptorY - speed);
+            }else{
+                interceptor.setY(interceptorY + speed);
+            }
+        }else{
             PlaneCoordinator.removePlane(interceptor.getPlane());
         }
+
+
+
     }
 
     private int calculateDistance() {
@@ -39,6 +74,16 @@ public class InterceptorTrackTask extends TimerTask {
         int targetX = target.getPosition().getX();
         int targetY = target.getPosition().getY();
         return (int) Math.sqrt(Math.pow(interceptorX - targetX, 2) + Math.pow(interceptorY - targetY, 2));
+    }
+
+    private int calculateDistanceToAirBase(){
+        int interceptorX = interceptor.getPosition().getX();
+        int interceptorY = interceptor.getPosition().getY();
+        int airbaseX = ((InterceptorPlane)interceptor.getPlane()).getAirbase().getPosition().getX();
+        int airBaseY= ((InterceptorPlane)interceptor.getPlane()).getAirbase().getPosition().getY();
+
+        return (int) Math.sqrt(Math.pow(interceptorX-airbaseX,2) + Math.pow(interceptorY-airBaseY,2));
+
     }
 
     @Override
